466fe1dc294b3d87b1e5f0fa87302f1b7b1d59c1
tophsm <67989403+tophsm@users.noreply.github.com>
2022-02-27 21:29:32 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,14 +1,4 @@
-#Project 1
-#Learning String Concatenation to play "madlibs"
-#first fill in: "Subscribe to _____"
-#youtuber = "" #youtuber will fill in the blank
-
-#ways to print statement with value (youtuber)
-#print("Subscribe to " + youtuber)
-#print("Subscribe to {}".format(youtuber))
-#print(f"Subscribe to {youtuber}")
-
-#My Own Madlib
+#Practicing String Concatenation to play Madlibs
 noun1 = input("Enter a noun: ")
 noun2 = input("Enter a noun: ")
 emotion = input("Enter an emotion ending in '-ly': ")
